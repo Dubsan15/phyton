287ffa060a9e87c02d3c846e0866be794ae953e8
Dubsan15 <159148375+Dubsan15@users.noreply.github.com>
2024-02-13 20:54:18 -0500

    Add files via upload

---
+++ Trabajo en casa.py
@@ -0,0 +1,13 @@
+Corte1=0.72
+corte2=1.01
+corte3=1.45
+
+resultado=Corte1+corte2+corte3
+print("el resultado de la nota final es:", resultado)
+
+
+base=3
+hipotenusa=5
+
+resultado=base*hipotenusa/2
+print("El area del triangulo rectangulo es:",resultado)
