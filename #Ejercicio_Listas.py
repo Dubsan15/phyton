bd081e9d385553e87c21d844b08a9fbd2693ba86
Dubsan15 <159148375+Dubsan15@users.noreply.github.com>
2024-02-21 09:34:35 -0500

    Add files via upload

---
+++ #Ejercicio_Listas.py
@@ -0,0 +1,21 @@
+#Ejercicio_Listas
+valor_a = 5
+#              0  1  2  3  4   5
+valor_lista = [4, 7, 8, 9, 10, 20, 30, 40, 55]
+print(valor_lista[2])
+
+#Ejercicio_listas_Texto
+lista_2 = ["a","e", "i", "o", "u"]
+print(lista_2[4])
+
+#Ejercicio_listas_aplicando_append
+lista_2.append("prueba")
+print(lista_2)
+
+#Ejercicio_Listas_Aplicando_Pop
+lista_2.pop()
+print(lista_2)
+
+#Ejercicio_Listas_Aplicando_Clear
+lista_2.clear()
+print(lista_2)
